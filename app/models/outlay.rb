cb9078dc97690653977f751f98b5e44faa157dda
FrederickMih <fredtimermih@gmail.com>
2021-05-07 02:49:26 +0100

    Add associations and validations for the outlay model

--- app/models/outlay.rb
+++ app/models/outlay.rb
@@ -1,2 +1,10 @@
 class Outlay < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :outlays_groups, dependent: :destroy
+  has_many :groups, through: :outlays_groups
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 60 }
+  validates :amount, presence: true, numericality: { less_than: 20_000 }
+
+  scope :recent_first, -> { order('creation_date DESC') }
 end
