4fce6349558d3770164dae0342a2484759e1e2d2
FrederickMih <fredtimermih@gmail.com>
2021-05-07 01:21:18 +0100

    Configure pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_06_235132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
